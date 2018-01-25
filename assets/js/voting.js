dc123c99e59c0d33ae5f1438f28040a017943ecd
Michael Bolens <bolensmichael@gmail.com>
2018-01-24 18:59:09 -0700

    Minor rebasing

--- assets/js/voting.js
+++ assets/js/voting.js
@@ -33,27 +33,27 @@ database.ref("/votes").on("value", function(snap){
 });
 
 $(".card").on("click", function (event) {
-  var currentCard = $cards[parseInt($(this).data("card"))];
+  var currentCard = $cards[parseInt($(this).data("votes"))];
   currentCard++;
-  selectedCard = parseInt($(this).data("card"));
+  selectedCard = parseInt($(this).data("votes"));
 
   if (selectedCard === 0) {
     database.ref("/votes").set({
       card1: currentCard,
       card2: $cards[1],
       card3: $cards[2]
-    })
+    });
   } else if (selectedCard === 1) {
     database.ref("/votes").set({
       card2: currentCard,
       card1: $cards[0],
       card3: $cards[2]
-    })
+    });
   } else if (selectedCard === 2) {
     database.ref("/votes").set({
       card3: currentCard,
       card1: $cards[0],
       card2: $cards[1]
-    })
+    });
   }
-})
+});
